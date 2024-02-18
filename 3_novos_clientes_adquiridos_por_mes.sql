def2f6d6f6db20b38363e9c1748d496e0337c83c
Giovani <106926901+GiovaniF25@users.noreply.github.com>
2024-02-18 19:33:14 -0300

    Update 3_novos_clientes_adquiridos_por_mes.sql

--- 3_novos_clientes_adquiridos_por_mes.sql
+++ 3_novos_clientes_adquiridos_por_mes.sql
@@ -1,11 +1,17 @@
-SELECT
-    EXTRACT(MONTH FROM MIN(rental.rental_date)) AS month,
-    COUNT(DISTINCT customer.customer_id) AS new_customers
-FROM
-    customer
-JOIN
-    rental ON customer.customer_id = rental.customer_id
-GROUP BY
-    EXTRACT(MONTH FROM rental.rental_date)
-ORDER BY
-    month;
+select
+    extract(month from min(rental.rental_date)) as month,
+    count(distinct customer.customer_id) as new_customers
+from customer
+join rental on customer.customer_id = rental.customer_id
+group by extract(month from rental.rental_date)
+order by month
+;
+
+
+#month | new_customers 
+#-------+---------------
+#     2 |           158
+#     5 |           520
+#     6 |           590
+#     7 |           599
+#     8 |           599
